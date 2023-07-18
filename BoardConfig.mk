41a0426f28904d298a81299d3db807b5c0468da7
kissoka <orangefoxci73@gmail.com>
2023-07-18 17:56:34 +0200

    agate: Write OTA assert flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,8 @@
 DEVICE_PATH := device/xiaomi/agate
 
+# Assertation
+TARGET_OTA_ASSERT_DEVICE := agate,agatein,amber
+
 # Inherit from common trees
 include device/xiaomi/mt6893-common/BoardConfigCommon.mk
 
